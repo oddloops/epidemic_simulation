a72a7e810fdeecbda1082e59eb030fce968a85b2
Vinci Wu <wu.vinci00@gmail.com>
2023-08-08 17:47:53 -0400

    Instantiated an Individual object and tested getter and setter outputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include "Individual.h"
+#include "Enums.h"
+#include "Utils.h"
 
-int main() {
-	std::cout << "Test" << std::endl;
+int main()
+{
+	Individual test(1, 50, HealthStatus::Normal, InfectionStatus::Susceptible);
 
+	// Test if declared correctly
+	std::cout << "ID: " << test.getID() << std::endl;
+	std::cout << "Age: " << test.getAge() << std::endl;
+	std::cout << "Health Status: " << static_cast<int>(test.getHealthStatus()) << std::endl;
+	std::cout << "Infection Status: " << static_cast<int>(test.getInfectionStatus()) << std::endl;
+
+	// Test setters
+	test.updateHealthStatus(HealthStatus::Infected);
+	test.updateInfectionStatus(InfectionStatus::CriticalCondition);
+
+	// Output object attributes
+	std::cout << "New Health Status: " << static_cast<int>(test.getHealthStatus()) << std::endl;
+	std::cout << "New Infection Status: " << static_cast<int>(test.getInfectionStatus()) << std::endl;
 	return 0;
 }
