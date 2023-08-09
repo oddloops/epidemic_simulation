4bdaeee2822cf022aecc2435f3f8c857353570d6
Vinci Wu <wu.vinci00@gmail.com>
2023-08-08 22:50:40 -0400

    Created a population of Individual objects of N

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
+#include <cstdlib>
 #include "Individual.h"
 #include "Enums.h"
 #include "Utils.h"
 
+constexpr int N = 1000;
+
 int main()
 {
-	Individual test(1, 50, HealthStatus::Normal, InfectionStatus::Susceptible);
+	// Generate population
+	std::vector<Individual> population;
 
-	// Test if declared correctly
-	std::cout << "ID: " << test.getID() << std::endl;
-	std::cout << "Age: " << test.getAge() << std::endl;
-	std::cout << "Health Status: " << static_cast<int>(test.getHealthStatus()) << std::endl;
-	std::cout << "Infection Status: " << static_cast<int>(test.getInfectionStatus()) << std::endl;
-
-	// Test setters
-	test.updateHealthStatus(HealthStatus::Infected);
-	test.updateInfectionStatus(InfectionStatus::CriticalCondition);
-
-	// Output object attributes
-	std::cout << "New Health Status: " << static_cast<int>(test.getHealthStatus()) << std::endl;
-	std::cout << "New Infection Status: " << static_cast<int>(test.getInfectionStatus()) << std::endl;
+	for (int i = 0; i < N; i++) 
+	{
+		int age = std::rand() % 100;
+		population.push_back(Individual(i + 1, age, HealthStatus::Normal, InfectionStatus::Susceptible));
+	}
+	
 	return 0;
 }
