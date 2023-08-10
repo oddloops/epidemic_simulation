6ab3f9949af563e1cd660f78bc8094670ba5ca66
Vinci Wu <wu.vinci00@gmail.com>
2023-08-10 16:06:39 -0400

    Added additional attribute for Individual class

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Individual.h"
 #include "Enums.h"
 #include "Utils.h"
+#include "Virus.h"
 
 constexpr int N = 1000;
 
@@ -11,11 +12,10 @@ int main()
 	// Generate population
 	std::vector<Individual> population;
 
-	for (int i = 0; i < N; i++) 
+	for (int i = 0; i < N; i++)
 	{
 		int age = std::rand() % 100;
-		population.push_back(Individual(i + 1, age, HealthStatus::Normal, InfectionStatus::Susceptible));
+		population.push_back(Individual(i + 1, age, HealthStatus::Normal, InfectionStatus::Susceptible, 0));
 	}
-	
 	return 0;
 }
