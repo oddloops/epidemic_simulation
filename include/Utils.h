adb6525961fc00dfb073d724ded700283ecaf08e
Vinci Wu <wu.vinci00@gmail.com>
2023-08-11 17:32:43 -0400

    Defined a template function to handle user inputs

--- include/Utils.h
+++ include/Utils.h
@@ -2,9 +2,39 @@
 #ifndef UTIL
 #define UTIL
 
+#include <iostream>
+#include <limits>
 #include <string>
 #include <vector>
 #include "Enums.h"
+
+// Process user input functions
+template <typename T>
+T getInputData(const std::string& prompt, T minValue, T maxValue)
+{
+	T inputValue;
+	while (true)
+	{
+		std::cout << prompt;
+		if (std::cin >> inputValue)
+		{
+			if (inputValue >= minValue && inputValue <= maxValue)
+			{
+				break;
+			}
+			else
+			{
+				std::cout << "Invalid Input. Please enter an input from " << minValue << " to " << maxValue << "." << std::endl;
+			}
+		}
+		else {
+			std::cout << "Invalid input. Please enter a valid number." << std::endl;
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+	return inputValue;
+}
 
 std::string healthToString(HealthStatus status);
 std::string infectionToString(InfectionStatus status);
