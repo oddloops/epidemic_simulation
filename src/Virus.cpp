0e57b85067e2c6d1bdac2fceec9b07759d545d00
Vinci Wu <wu.vinci00@gmail.com>
2023-08-11 00:14:18 -0400

    Removed switch case and replaced them with if conditionals and defined the infect method

--- src/Virus.cpp
+++ src/Virus.cpp
@@ -19,20 +19,19 @@ void Virus::mutate()
 	{
 		std::random_device randomGen;
 		std::mt19937 gen(randomGen());
-		std::uniform_int_distribution<> mutation(1, 3);
-		switch (mutation(gen))
+		std::uniform_real_distribution<> decimalDis(0.0, 1.0);
+		double mutationChance = decimalDis(gen);
+		if (mutationChance <= 0.25)
 		{
-		case 1:
 			transmissionRate += 0.2;
-			break;
-		case 2:
+		}
+		else if (mutationChance <= 0.5)
+		{
 			incubationPeriod -= 1;
-			break;
-		case 3:
+		}
+		else if (mutationChance <= 0.75)
+		{
 			recoveryPeriod += 2;
-			break;
-		default:
-			break;
 		}
 	}
 }
@@ -58,65 +57,43 @@ void Virus::severity(Individual& individual)
 {
 	if (individual.getHealthStatus() != HealthStatus::Deceased)
 	{
-		if (individual.getDaysInfected() >= recoveryPeriod)
+		int daysInfected = individual.getDaysInfected();
+
+		if (daysInfected >= recoveryPeriod)
 		{
-			individual.updateHealthStatus(HealthStatus::Normal);
+			individual.updateHealthStatus(HealthStatus::Recovered);
 			individual.updateInfectionStatus(InfectionStatus::Recovered);
 		}
-		else if (individual.getDaysInfected() >= incubationPeriod)
+		else if (daysInfected >= incubationPeriod)
 		{
-			std::random_device randomGen;
-			std::mt19937 gen(randomGen());
-			std::uniform_real_distribution<> decimalDis(0.0, 1.0);
-			if (individual.getInfectionStatus() != InfectionStatus::Asymptomatic)
+			double infectionSeverity = static_cast<double>(daysInfected - incubationPeriod) / (recoveryPeriod - incubationPeriod);
+
+			if (infectionSeverity < 0.25)
 			{
-				double infectionSeverity = decimalDis(gen);
-				InfectionStatus severity = individual.getInfectionStatus();
-				switch (severity)
-				{
-					case InfectionStatus::CriticalCondition:
-						if (infectionSeverity < lethality)
-						{
-							individual.updateHealthStatus(HealthStatus::Deceased);
-							individual.updateInfectionStatus(InfectionStatus::Deceased);
-						}
-						break;
-					case InfectionStatus::MildSymptoms:
-						if (infectionSeverity < 0.35)
-						{
-							individual.updateInfectionStatus(InfectionStatus::SevereSymptoms);
-						}
-						break;
-					case InfectionStatus::SevereSymptoms:
-						if (infectionSeverity < 0.28)
-						{
-							individual.updateInfectionStatus(InfectionStatus::CriticalCondition);
-						}
-						break;
-					default:
-						break;
-				}
+				individual.updateInfectionStatus(InfectionStatus::MildSymptoms);
 			}
-			else {
-				std::uniform_int_distribution<> dis(1, 5);
-				int infectionSeverity = dis(gen);
-				switch (infectionSeverity)
-				{
-				case 1:
-					individual.updateInfectionStatus(InfectionStatus::Symptomatic);
-					break;
-				case 2:
-					individual.updateInfectionStatus(InfectionStatus::MildSymptoms);
-					break;
-				case 3:
-					individual.updateInfectionStatus(InfectionStatus::SevereSymptoms);
-					break;
-				default:
-					break;
-				}
+			else if (infectionSeverity < 0.5)
+			{
+				individual.updateInfectionStatus(InfectionStatus::SevereSymptoms);
+			}
+			else if (infectionSeverity < 0.75)
+			{
+				individual.updateInfectionStatus(InfectionStatus::CriticalCondition);
+			}
+			else
+			{
+				individual.updateHealthStatus(HealthStatus::Deceased);
+				individual.updateInfectionStatus(InfectionStatus::Deceased);
 			}
 		}
 	}
+}
+
+
+void Virus::infect(Individual& individual)
+{
+	individual.updateHealthStatus(HealthStatus::Infected);
+	individual.updateInfectionStatus(InfectionStatus::Asymptomatic);
 }
 
 double Virus::getTranmissionRate()
