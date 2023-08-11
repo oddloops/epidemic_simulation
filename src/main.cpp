2bce49c229179c5a77cdb663c2bedcddd80b4846
Vinci Wu <wu.vinci00@gmail.com>
2023-08-11 15:50:46 -0400

    Added code to prompt the user for population size, simulation duration, and virus properties

--- src/main.cpp
+++ src/main.cpp
@@ -15,48 +15,138 @@ int main()
     int simulationData[3] = { 0 };
 
     std::vector<Individual> population;
-    int i;
-    for (i = 0; i < N; i++)
+
+    // User inputs
+    int i, populationSize = N, simulationDays = SIMULATION_DAYS;
+
+    // Virus user inputs
+    double transmissionRate = 0, mutationChance = 0, lethality = 0;
+    int incubationPeriod = 0, recoveryPeriod = 0;
+
+    // Parse user inputs (Population, Simulation Days then virus information)
+    // Population Size
+    do
+    {
+        std::cout << "Population Size: ";
+        std::cin >> populationSize;
+        if (populationSize < 1)
+        {
+            std::cout << "Please input a valid value > 0" << std::endl;
+        }
+    } while (populationSize < 1);
+
+    // Simulation Time (in days)
+    do
+    {
+        std::cout << "Simulation Days: ";
+        std::cin >> simulationDays;
+        if (simulationDays < 1)
+        {
+            std::cout << "Please input a value > 0" << std::endl;
+        }
+    } while (simulationDays < 1);
+
+    // Virus attributes
+    // Transmission Rate
+    do
+    {
+        std::cout << "Virus transmission rate (0 to 1): ";
+        std::cin >> transmissionRate;
+        if (transmissionRate < 0 || transmissionRate > 1)
+        {
+            std::cout << "Please input a value between 0 to 1" << std::endl;
+        }
+    } while (transmissionRate < 0 || transmissionRate > 1);
+
+    // Incubation Period
+    do
+    {
+        std::cout << "Virus Incubation rate (in days): ";
+        std::cin >> incubationPeriod;
+        if (incubationPeriod < 0)
+        {
+            std::cout << "Please input a value >= 0" << std::endl;
+        }
+    } while (incubationPeriod < 0 );
+
+    // Recovery Period
+    do
+    {
+        std::cout << "Virus Recovery rate (in days): ";
+        std::cin >> recoveryPeriod;
+        if (recoveryPeriod < 0)
+        {
+            std::cout << "Please input a value >= 0" << std::endl;
+        }
+    } while (recoveryPeriod < 0);
+
+    // Mutation Chance
+    do
+    {
+        std::cout << "Virus Mutation Chance (0 to 1): ";
+        std::cin >> mutationChance;
+        if (mutationChance < 0 || mutationChance > 1)
+        {
+            std::cout << "Please input a value between 0 to 1" << std::endl;
+        }
+    } while (mutationChance < 0 || mutationChance > 1);
+
+    // Lethality
+    do
+    {
+        std::cout << "Virus Lethality Rate (0 to 1): ";
+        std::cin >> lethality;
+        if (lethality < 0 || lethality > 1)
+        {
+            std::cout << "Please input a value between 0 to 1" << std::endl;
+        }
+    } while (lethality < 0 || lethality > 1);
+
+    // Set up population
+    for (i = 0; i < populationSize; i++)
     {
         int age = std::rand() % 100;
         population.push_back(Individual(i + 1, age, HealthStatus::Normal, InfectionStatus::Susceptible, 0));
     }
 
-    Virus test(0.31, 3, 8, 0.02, 0.11);
+    // Set up virus
+    Virus virus(transmissionRate, incubationPeriod, recoveryPeriod, mutationChance, lethality);
 
-    // Randomly infect an individual
+    // Set up random number generators
     std::random_device randomDevice;
     std::mt19937 gen(randomDevice());
-    std::uniform_int_distribution<> individuals(0, N - 1);
+    std::uniform_int_distribution<> individuals(0, populationSize - 1);
     std::uniform_real_distribution<> randomInfection(0, 1);
+
+    // Randomly infect an individual
     int randomInfected = individuals(gen); // Generate a random index
-    test.infect(population[randomInfected]); // Use your Virus.infect() method
+    virus.infect(population[randomInfected]); // Use your Virus.infect() method
 
     // Begin the simulation loop
-    for (i = 0; i <= SIMULATION_DAYS; i++)
+    for (i = 0; i <= simulationDays; i++)
     {
-        for (int k = 0; k < N; k++) {
+        for (int k = 0; k < populationSize; k++) {
             Individual& person = population[k]; // Use reference
 
             if (person.getHealthStatus() == HealthStatus::Infected)
             {
-                test.severity(person);
+                virus.severity(person);
                 person.incrementInfectionDays();
 
                 if (k - 1 >= 0)
                 {
-                    test.spreadTo(population[k - 1]);
+                    virus.spreadTo(population[k - 1]);
                 }
-                if (k + 1 < N)
+                if (k + 1 < populationSize)
                 {
-                    test.spreadTo(population[k + 1]);
+                    virus.spreadTo(population[k + 1]);
                 }
             }
             else {
                 double randomInfectChance = randomInfection(gen);
                 if (randomInfectChance < 0.0000002)
                 {
-                    test.infect(person);
+                    virus.infect(person);
                 }
             }
         }
@@ -80,8 +170,8 @@ int main()
             }
         }
     }
-    std::cout << "Infected Total: " << simulationData[0] << " / " << N << std::endl;
-    std::cout << "Recovered Total: " << simulationData[1] << " / " << N << std::endl;
-    std::cout << "Deceased Total: " << simulationData[2] << " / " << N << std::endl;
+    std::cout << "Estimated Infected Total: " << simulationData[0] << " / " << N << std::endl;
+    std::cout << "Estimated Recovered Total: " << simulationData[1] << " / " << N << std::endl;
+    std::cout << "Estimated Deceased Total: " << simulationData[2] << " / " << N << std::endl;
     return 0;
 }
