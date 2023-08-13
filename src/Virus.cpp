974b85b39d9709f857c6e5242d8852ecf98d7c71
Vinci Wu <wu.vinci00@gmail.com>
2023-08-13 17:33:01 -0400

    Reorganized code by removing redundant random generators and incorporate constants and lethality

--- src/Virus.cpp
+++ src/Virus.cpp
@@ -1,122 +1,118 @@
 #include "Virus.h"
 
 Virus::Virus(double rateTransmission, int incubatePeriod, int recoverPeriod, double mutation, double lethalityP)
+    : transmissionRate(rateTransmission), incubationPeriod(incubatePeriod),
+    recoveryPeriod(recoverPeriod), mutationChance(mutation), lethality(lethalityP),
+    randomGen(), gen(randomGen()), decimalDis(0.0, 1.0)
 {
-	transmissionRate = rateTransmission;
-	incubationPeriod = incubatePeriod;
-	recoveryPeriod = recoverPeriod;
-	mutationChance = mutation;
-	lethality = lethalityP;
 }
 
 void Virus::mutate()
 {
-	std::random_device randomGen;
-	std::mt19937 gen(randomGen());
-	std::uniform_real_distribution<> decimalDis(0.0, 1.0);
-	double mutationP = decimalDis(gen);
-	if (mutationChance <= mutationP)
-	{
-		std::random_device randomGen;
-		std::mt19937 gen(randomGen());
-		std::uniform_real_distribution<> decimalDis(0.0, 1.0);
-		double mutationChance = decimalDis(gen);
-		if (mutationChance <= 0.25)
-		{
-			transmissionRate += 0.2;
-		}
-		else if (mutationChance <= 0.5)
-		{
-			incubationPeriod -= 1;
-		}
-		else if (mutationChance <= 0.75)
-		{
-			recoveryPeriod += 2;
-		}
-	}
+    double mutationP = decimalDis(gen);
+    if (mutationChance <= mutationP)
+    {
+        double mutationChanceValue = decimalDis(gen);
+        if (mutationChanceValue <= MUTATION_TRANSMISSION)
+        {
+            transmissionRate += 0.2;
+        }
+        else if (mutationChanceValue <= MUTATION_INCUBATION)
+        {
+            incubationPeriod -= 1;
+        }
+        else if (mutationChanceValue <= MUTATION_RECOVERY)
+        {
+            recoveryPeriod += 2;
+        }
+    }
 }
 
 void Virus::spreadTo(Individual& individual)
 {
-	if (individual.getInfectionStatus() == InfectionStatus::Susceptible)
-	{
-		double infectionChance = transmissionRate;
-		std::random_device randomGen;
-		std::mt19937 gen(randomGen());
-		std::uniform_real_distribution<> decimalDis(0.0, 1.0);
-		double randomChance = decimalDis(gen);
-		if (randomChance < infectionChance)
-		{
-			individual.updateHealthStatus(HealthStatus::Infected);
-			individual.updateInfectionStatus(InfectionStatus::Asymptomatic);
-		}
-	}
+    if (individual.getInfectionStatus() == InfectionStatus::Susceptible)
+    {
+        double infectionChance = transmissionRate;
+        double randomChance = decimalDis(gen);
+        if (randomChance < infectionChance)
+        {
+            individual.updateHealthStatus(HealthStatus::Infected);
+            individual.updateInfectionStatus(InfectionStatus::Asymptomatic);
+        }
+    }
 }
 
 void Virus::severity(Individual& individual)
 {
-	if (individual.getHealthStatus() != HealthStatus::Deceased)
-	{
-		int daysInfected = individual.getDaysInfected();
+    if (individual.getHealthStatus() != HealthStatus::Deceased)
+    {
+        int daysInfected = individual.getDaysInfected();
 
-		if (daysInfected >= recoveryPeriod)
-		{
-			individual.updateHealthStatus(HealthStatus::Recovered);
-			individual.updateInfectionStatus(InfectionStatus::Recovered);
-		}
-		else if (daysInfected >= incubationPeriod)
-		{
-			double infectionSeverity = static_cast<double>(daysInfected - incubationPeriod) / (recoveryPeriod - incubationPeriod);
+        if (daysInfected >= recoveryPeriod)
+        {
+            individual.updateHealthStatus(HealthStatus::Recovered);
+            individual.updateInfectionStatus(InfectionStatus::Recovered);
+        }
+        else if (daysInfected >= incubationPeriod)
+        {
+            double infectionSeverity = static_cast<double>(daysInfected - incubationPeriod) / (recoveryPeriod - incubationPeriod);
 
-			if (infectionSeverity < 0.25)
-			{
-				individual.updateInfectionStatus(InfectionStatus::MildSymptoms);
-			}
-			else if (infectionSeverity < 0.5)
-			{
-				individual.updateInfectionStatus(InfectionStatus::SevereSymptoms);
-			}
-			else if (infectionSeverity < 0.75)
-			{
-				individual.updateInfectionStatus(InfectionStatus::CriticalCondition);
-			}
-			else
-			{
-				individual.updateHealthStatus(HealthStatus::Deceased);
-				individual.updateInfectionStatus(InfectionStatus::Deceased);
-			}
-		}
-	}
+            double lethalityChance = lethality * infectionSeverity; // Calculate lethality chance based on infection severity
+
+            if (decimalDis(gen) < lethalityChance)
+            {
+                individual.updateHealthStatus(HealthStatus::Deceased);
+                individual.updateInfectionStatus(InfectionStatus::Deceased);
+            }
+            else if (infectionSeverity < SEVERITY_MILD)
+            {
+                individual.updateInfectionStatus(InfectionStatus::MildSymptoms);
+            }
+            else if (infectionSeverity < SEVERITY_SEVERE)
+            {
+                individual.updateInfectionStatus(InfectionStatus::SevereSymptoms);
+            }
+            else if (infectionSeverity < SEVERITY_CRITICAL)
+            {
+                individual.updateInfectionStatus(InfectionStatus::CriticalCondition);
+            }
+            else // infectionSeverity >= SEVERITY_CRITICAL
+            {
+                individual.updateHealthStatus(HealthStatus::Deceased);
+                individual.updateInfectionStatus(InfectionStatus::Deceased);
+            }
+        }
+    }
 }
 
 
 void Virus::infect(Individual& individual)
 {
-	individual.updateHealthStatus(HealthStatus::Infected);
-	individual.updateInfectionStatus(InfectionStatus::Asymptomatic);
+    individual.updateHealthStatus(HealthStatus::Infected);
+    individual.updateInfectionStatus(InfectionStatus::Asymptomatic);
 }
 
 double Virus::getTranmissionRate()
 {
-	return transmissionRate;
+    return transmissionRate;
 }
 
 int Virus::getIncubationRate()
 {
-	return incubationPeriod;
+    return incubationPeriod;
 }
 
 int Virus::getRecoveryPeriod()
 {
-	return recoveryPeriod;
+    return recoveryPeriod;
 }
 
 double Virus::getMutationChance()
 {
-	return mutationChance;
+    return mutationChance;
 }
 
 double Virus::getLethality()
 {
-	return lethality;
+    return lethality;
 }
